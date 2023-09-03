e490e870aed0a6d8bb929f2255ea1d12e9fe946f
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-09-03 16:31:24 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req,res)=>{
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('My name is sunil');
 });
-
+ 
 server.listen(3000,'127.0.0.1',()=>{
   console.log("Server is running")
 });
