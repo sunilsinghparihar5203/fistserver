d4cba2efc7c52d54689d7525c8efd42e60ae2960
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-09-04 22:05:22 +0530

    read and write message.txt

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
   const url = req.url;
   const method = req.method;
-  if(url === '/home' || url === '/'){
-    res.write('<h1>Welcome home</h1>')
+  if(url === '/'){
+    const text = fs.readFileSync('message.txt')
+    res.write('<html><p>'+ text +'</p><form action="/message" method="post"><input type="text" name="message" /> <input type="submit" /></form></html>')
     return res.end();
   }
-  else if(url === '/about'){
-    res.write('<h1>Welcome to about us page.</h1>')
-    return res.end();
+  if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data',(chunk)=>{
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end',()=>{
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody)
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync('message.txt',message);
+    })
+    
+    res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end()
   }
-  else if(url === '/node'){
-    res.write('<h1>Welcome To projects</h1>')
-    return res.end();
-  }
-  else{
-    res.write('<h1>Page does\'nt exist.</h1>')
-    return res.end();
-  }
+  res.setHeader('Content-type','text/html');
+  res.write('<h1>This is message page</h1>')
+  res.end();
 });
  
 server.listen(3000,'127.0.0.1',()=>{
