6c77d0c2936fc50a7bad4d32d5b613bc3e8015be
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-09-04 21:28:16 +0530

    basic routing done

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('My name is sunil');
+  const url = req.url;
+  const method = req.method;
+  if(url === '/home' || url === '/'){
+    res.write('<h1>Welcome home</h1>')
+    return res.end();
+  }
+  else if(url === '/about'){
+    res.write('<h1>Welcome to about us page.</h1>')
+    return res.end();
+  }
+  else if(url === '/node'){
+    res.write('<h1>Welcome To projects</h1>')
+    return res.end();
+  }
+  else{
+    res.write('<h1>Page does\'nt exist.</h1>')
+    return res.end();
+  }
 });
  
 server.listen(3000,'127.0.0.1',()=>{
