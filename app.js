bc0b5f120e55b743a24d0321b23373e7c2575ed5
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-09-05 10:49:59 +0530

    exported module all 3 ways

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
 const http = require('http');
-const fs = require('fs')
 
-const server = http.createServer((req,res)=>{
-  const url = req.url;
-  const method = req.method;
-  if(url === '/'){
-    const text = fs.readFileSync('message.txt')
-    res.write('<html><p>'+ text +'</p><form action="/message" method="post"><input type="text" name="message" /> <input type="submit" /></form></html>')
-    return res.end();
-  }
-  if(url === '/message' && method === 'POST'){
-    const body = [];
-    req.on('data',(chunk)=>{
-      console.log(chunk)
-      body.push(chunk)
-    })
-    req.on('end',()=>{
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody)
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync('message.txt',message);
-    })
-    
-    res.statusCode = 302;
-    res.setHeader('Location','/');
-    return res.end()
-  }
-  res.setHeader('Content-type','text/html');
-  res.write('<h1>This is message page</h1>')
-  res.end();
-});
+const routes = require('./routes')
+
+const server = http.createServer(routes.handler);
  
 server.listen(3000,'127.0.0.1',()=>{
   console.log("Server is running")
---
+++ routes.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+const requestHandler = (req,res) =>{
+  const url = req.url;
+  const method = req.method;
+  if(url === '/'){
+    const text = fs.readFileSync('message.txt')
+    res.write('<html><p>'+ text +'</p><form action="/message" method="post"><input type="text" name="message" /> <input type="submit" /></form></html>')
+    return res.end();
+  }
+  if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data',(chunk)=>{
+      console.log(chunk)
+      body.push(chunk)
+    })
+   return  req.on('end',()=>{
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody)
+      const message = parsedBody.split("=")[1];
+      fs.writeFile('message.txt',message,(err)=>{
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end()
+      }); 
+    })
+  }
+  res.setHeader('Content-type','text/html');
+  res.write('<h1>This is message page</h1>')
+  res.end();
+}
+
+// module.exports  = requestHandler
+
+// module.exports = {
+//   handler : requestHandler,
+//   sometext: "Some hard coded text"
+// };
+
+module.exports.handler = requestHandler
+module.exports.sometext = "some hard coded text"
