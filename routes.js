95ded896b42eaeb83484e16d5d037ff94f5ef22b
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-09-05 12:47:55 +0530

    debugging done

--- routes.js
+++ routes.js
@@ -1,34 +1,38 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const requestHandler = (req,res) =>{
+const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
-  if(url === '/'){
-    const text = fs.readFileSync('message.txt')
-    res.write('<html><p>'+ text +'</p><form action="/message" method="post"><input type="text" name="message" /> <input type="submit" /></form></html>')
+  if (url === "/") {
+    const text = fs.readFileSync("message.txt");
+    res.write(
+      "<html><p>" +
+        text +
+        '</p><form action="/message" method="post"><input type="text" name="message" /> <input type="submit" /></form></html>'
+    );
     return res.end();
   }
-  if(url === '/message' && method === 'POST'){
+  if (url === "/message" && method === "POST") {
     const body = [];
-    req.on('data',(chunk)=>{
-      console.log(chunk)
-      body.push(chunk)
-    })
-   return  req.on('end',()=>{
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody)
+      console.log(parsedBody);
       const message = parsedBody.split("=")[1];
-      fs.writeFile('message.txt',message,(err)=>{
+      fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end()
-      }); 
-    })
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
-  res.setHeader('Content-type','text/html');
-  res.write('<h1>This is message page</h1>')
+  res.setHeader("Content-type", "text/html");
+  res.write("<h1>This is message page</h1>");
   res.end();
-}
+};
 
 // module.exports  = requestHandler
 
@@ -37,5 +41,5 @@ const requestHandler = (req,res) =>{
 //   sometext: "Some hard coded text"
 // };
 
-module.exports.handler = requestHandler
-module.exports.sometext = "some hard coded text"
+module.exports.handler = requestHandler;
+module.exports.sometext = "some hard coded text";
